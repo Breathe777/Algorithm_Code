a56acf1d80d6fac6f10e4db8d8db30a22055660c
Jess <108989281+Breathe777@users.noreply.github.com>
2023-04-06 14:27:11 +0800

    Update All in.cpp

--- Algorithm template/All in.cpp
+++ Algorithm template/All in.cpp
@@ -35,7 +35,39 @@ int main()
 } 
 
 
+//归并排序
+#include <iostream>
+using namespace std;
 
+const int N = 100010;
+
+int tmp[N];
+int n;
+
+void merge_sort(int q[], int left, int right)
+{
+    if(left >= right) return;
+    int mid = (left + right) >> 1;
+    merge_sort(q, left, mid);
+    merge_sort(q, mid + 1, right);
+    int k = 0, i = left, j = mid + 1;
+    while(i <= mid && j <= right)
+    {
+        if(q[i] <= q[j]) tmp[k++] = q[i++];
+        else tmp[k++] = q[j++];
+    }
+    while(i <= mid) tmp[k++] = q[i++];
+    while(j <= right) tmp[k++] = q[j++];
+    for(int i = left, j = 0;i <= right;i++, j++) q[i] = tmp[j];
+}
+int main()
+{
+    int q[N];
+    scanf("%d",&n);
+    for(int i = 0;i < n;i++) scanf("%d",&q[i]);
+    merge_sort(q,0,n-1);
+    for(int i = 0;i < n;i++) printf("%d ",q[i]);
+}
 
 
 
