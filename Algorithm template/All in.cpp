a9865b1af2ea5eb4fbc6058f24c91994e13e7e5f
Jess <108989281+Breathe777@users.noreply.github.com>
2023-04-10 21:49:10 +0800

    Update All in.cpp

--- Algorithm template/All in.cpp
+++ Algorithm template/All in.cpp
@@ -2096,3 +2096,86 @@ int main()
 }
 
 
+
+//lca最近公共祖先
+#include <iostream>
+#include <cstring>
+#include <queue>
+using namespace std;
+
+const int N = 40010, M = 2 * N;
+int h[N], e[M], ne[M], idx;
+int depth[N], fa[N][16];
+int n,m;
+
+void add(int a,int b)
+{
+    e[idx] = b, ne[idx] = h[a], h[a] = idx++;
+}
+void bfs(int root)
+{
+    memset(depth,0x3f,sizeof depth);
+    depth[0] = 0, depth[root] = 1;
+    queue<int>q;
+    q.push(root);
+    while(!q.empty())
+    {
+        int t = q.front();
+        q.pop();
+        for(int i = h[t];i != -1;i = ne[i])
+        {
+            int j = e[i];
+            if(depth[j] > depth[t] + 1)
+            {
+                depth[j] = depth[t] + 1;
+                q.push(j);
+                fa[j][0] = t;
+                for(int k = 1;k <= 15;k++) fa[j][k] = fa[fa[j][k-1]][k-1];
+            }
+        }
+    }
+}
+int lca(int a,int b)
+{
+    if(depth[a] < depth[b]) swap(a,b);
+    for(int k = 15;k >= 0;k--)
+    {
+        if(depth[fa[a][k]] >= depth[b])
+            a = fa[a][k];
+    }
+    if(a == b) return b;
+    for(int k = 15;k >=0;k--)
+    {
+        if(fa[a][k] != fa[b][k])
+        {
+            a = fa[a][k];
+            b = fa[b][k];
+        }
+    }
+    return fa[a][0];
+}
+int main()
+{
+    scanf("%d",&n);
+    memset(h,-1,sizeof h);
+    int root;
+    for(int i = 0;i < n;i++)
+    {
+        int a,b;
+        scanf("%d%d",&a,&b);
+        if(b == -1) root = a;
+        else add(a,b), add(b,a);
+    }
+    bfs(root);
+    scanf("%d",&m);
+    while(m--)
+    {
+        int x,y;
+        scanf("%d%d",&x,&y);
+        int p = lca(x,y);
+        if(p == x) puts("1");
+        else if(p == y) puts("2");
+        else puts("0");
+    }
+    return 0;
+}
